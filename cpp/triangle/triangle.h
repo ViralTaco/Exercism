ab3fefdbdb044a946a6226010fca3ef45d9d7bae
ViralTaco <anth@live.be>
2019-10-29 07:03:42 +0100

    Exercism C++ Triangle tests upgrade

--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef VT_TRIANGLE_H
+#define VT_TRIANGLE_H
 
 #ifndef EXERCISM_RUN_ALL_TESTS
 #define EXERCISM_RUN_ALL_TESTS
@@ -8,26 +9,32 @@
 
 namespace triangle {
 
-enum Triangle { equilateral, isosceles, scalene };
+enum class flavor: int { equilateral, isosceles, scalene };
 
-template <typename T>
-static const Triangle kind(const T side_a, const T side_b, const T side_c) noexcept(false) {
-  // Triangle inequality:
-  // For any triangle, the sum of the lengths of any two sides must be
-  // greater than or equal to the length of the remaining side.
-  if (side_a >= side_b + side_c or side_b >= side_a + side_c or side_c >= side_a + side_b) {
+template <typename T> flavor
+kind(const T a, const T b, const T side_c) {
+  /**
+   *  Triangle inequality:
+   *  For any triangle, the sum of the lengths of any two sides must be
+   *  greater than or equal to the length of the remaining side.
+   */
+  if (a >= b + side_c
+  or  b >= a + side_c
+  or  side_c >= a + b) {
     throw std::domain_error("Degenerate triangle or not a triangle.");
-  } else if (side_a <= 0 or side_b <= 0 or side_c <= 0) {
+  } else if (a <= 0 or b <= 0 or side_c <= 0) {
     throw std::domain_error("A triangle cannot have sides of length <= 0.");
   }
 
-  if (side_a == side_b and side_b == side_c) {
-    return triangle::equilateral;
-  } else if (side_a == side_b or side_a == side_c or side_b == side_c) {
-    return triangle::isosceles;
+  if (a == b and b == side_c) {
+    return flavor::equilateral;
+  } else if (a == b or a == side_c or b == side_c) {
+    return flavor::isosceles;
   }
 
-  return triangle::scalene;
+  return flavor::scalene;
 }
 
 } // namespace triangle
+
+#endif // VT_TRIANGLE_G (header guard)
